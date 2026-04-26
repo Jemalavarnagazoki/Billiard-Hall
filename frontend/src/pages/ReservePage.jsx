7ccf20b14a5f5ec9e7e3ac87cd6934755945221b
Jemalavarnagazoki <giorgishengeliajr@gmail.com>
2026-04-26 21:44:22 +0400

    Fix pricing bug: reset duration when switching between billiard and PlayStation

--- frontend/src/pages/ReservePage.jsx
+++ frontend/src/pages/ReservePage.jsx
@@ -108,6 +108,7 @@ export default function ReservePage() {
     setFormData((current) => ({
       ...current,
       reservationType: service,
+      durationHours: '1',
       reservationTime: ''
     }));
   }
